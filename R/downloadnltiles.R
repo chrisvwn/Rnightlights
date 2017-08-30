178a263bf1ba33e9f0d243e98c4f833f52ca00a0
Chris Njuguna <chrisvwn@yahoo.co.uk>
2017-08-30 21:56:13 +0300

    Updated aria2c downloadNlTilesOLS to correct the destination directory

--- R/downloadnltiles.R
+++ R/downloadnltiles.R
@@ -155,7 +155,8 @@ downloadNlTilesOLS <- function(nlYear, downloadMethod=pkgOptions("downloadMethod
     if (downloadMethod %in% c("auto", "curl", "libcurl", "wget"))
       rsltDnld <- utils::download.file(ntLtsFileUrl, ntLtsZipLocalNamePath, mode = "wb", method = downloadMethod, extra = "-c")
     else if (downloadMethod == "aria")
-      rsltDnld <- system(paste0("aria2c -c -x2 ", ntLtsFileUrl, " -d ", getNlDir("dirNlTiles"), " -o ", ntLtsZipLocalNamePath)) #downloads to path relative to -d if specified else local dir
+      rsltDnld <- system(paste0("aria2c -c -x2 ", ntLtsFileUrl, " -d ", getNlDir("dirNlTiles"), " -o ", getNlTileZipLclNameOLS(nlYear))) #downloads to path relative to -d if specified else local dir
+    
   }
   else
   {
