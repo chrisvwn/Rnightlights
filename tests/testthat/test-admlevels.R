0002d60789ba8470522e9c49a1d31b3fa7d5cbeb
Chris <chris.njuguna@gmail.com>
2018-05-26 00:04:19 +0300

    +Added tests for admLevel

---
+++ tests/testthat/test-admlevels.R
@@ -0,0 +1,14 @@
+library(testthat)
+library(Rnightlights)
+
+context("admLevels")
+
+test_that("admLevel lookups work", {
+  #ctryCodeToName
+  expect_equal(Rnightlights:::getCtryPolyAdmLevelNames("STP"), c("Municipality_(Concelho)", "District"), fixed=TRUE)
+  expect_equal(Rnightlights:::getCtryPolyAdmLevelNames("STP", "1"), c("Municipality_(Concelho)"), fixed=TRUE)
+  expect_equal(Rnightlights:::getCtryShpAllAdmLvls("STP"), list(STP=c("STP_adm0", "STP_adm1", "STP_adm2")), fixed=TRUE)
+  expect_equal(Rnightlights:::searchAdmLevel("STP", "Municipality"),list(STP="STP_adm1"), fixed=TRUE)
+  expect_equal(Rnightlights:::searchAdmLevel("STP", "highest"),list(STP="STP_adm1"), fixed=TRUE)
+  expect_true(Rnightlights:::validCtryAdmLvls("STP", c("District")))
+})
