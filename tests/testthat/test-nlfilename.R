6dd5beaee43c906307be287a880b85fbc1ec6819
chris <chris.njuguna@gmail.com>
2019-08-04 22:52:04 +0300

    +updated man

--- tests/testthat/test-nlfilename.R
+++ tests/testthat/test-nlfilename.R
@@ -4,35 +4,36 @@ library(testthat)
 context("nlfilename")
 
 test_that("nlfilenames are correct", {
-  expect_equal(Rnightlights:::getNlTileZipLclNamePath("VIIRS.M", "201401", 1), file.path(getNlDir("dirNlTiles"), paste0("NL_TILE_VIIRS.M_201401_", Rnightlights:::tileIdx2Name(tileNum = 1, nlType = "VIIRS.M"), ".tgz")))
+  expect_equal(Rnightlights:::getNlTileZipLclNamePath(nlType = "VIIRS.M", nlPeriod = "201401", tileNum = 1),
+               file.path(getNlDir("dirNlTiles"), "NL_TILE_VIIRS.M_VCMCFG_201401_75N180W.tgz"))
   
-  expect_equal(Rnightlights:::getNlTileZipLclNameVIIRS("201401", 1, "VIIRS.M"), "NL_TILE_VIIRS.M_201401_75N180W.tgz")
+  expect_equal(Rnightlights:::getNlTileZipLclNameVIIRS(nlPeriod = "201401", tileNum = 1, nlType = "VIIRS.M"), "NL_TILE_VIIRS.M_VCMCFG_201401_75N180W.tgz")
 
-  expect_equal(Rnightlights:::getNlTileTifLclNamePath("VIIRS.M", "201401", 1), file.path(getNlDir("dirNlTiles"), paste0("NL_TILE_VIIRS.M_201401_", Rnightlights:::tileIdx2Name(tileNum = 1, nlType = "VIIRS.M"), ".tif")))
+  expect_equal(Rnightlights:::getNlTileTifLclNamePath(nlType = "VIIRS.M", nlPeriod = "201401", tileNum = 1), file.path(getNlDir("dirNlTiles"), "NL_TILE_VIIRS.M_VCMCFG_201401_75N180W.tif"))
     
-  expect_equal(Rnightlights:::getNlTileTifLclNameVIIRS("201412", 1, "VIIRS.M"), "NL_TILE_VIIRS.M_201412_75N180W.tif")
+  expect_equal(Rnightlights:::getNlTileTifLclNameVIIRS(nlType = "VIIRS.M", nlPeriod = "201412", tileNum = 1), "NL_TILE_VIIRS.M_VCMCFG_201412_75N180W.tif")
   
-  expect_equal(Rnightlights:::getNlTileTifLclNameOLS("1992"), "NL_TILE_OLS.Y_1992_00N180W.tif")
+  expect_equal(Rnightlights:::getNlTileTifLclNameOLS(nlPeriod = "1992"), "NL_TILE_OLS.Y_STABLE_LIGHTS_1992_00N180W.tif")
   
-  expect_equal(Rnightlights:::getCtryNlDataFname(ctryCode = "KEN",
-                                                 admLevel = "KEN_adm0",
-                                                 gadmVersion = "2.8"),
-               "NL_DATA_KEN_ADM0_GADM-2.8.csv")
+  expect_equal(Rnightlights:::getCtryNlDataFname(ctryCode = "KEN", admLevel = "KEN_adm0", gadmVersion = "2.8"),
+               "NL_DATA_KEN_ADM0_GADM-2.8-SHPZIP.csv")
   
-  expect_equal(Rnightlights:::getPolyFname("KEN", gadmVersion = "2.8"),"SHP_KEN_GADM-2.8")
+  expect_equal(Rnightlights:::getPolyFname(ctryCode = "KEN", gadmVersion = "2.8"), "POLY_KEN_GADM-2.8-SHPZIP")
   
-  expect_equal(Rnightlights:::getPolyFnamePath("KEN", "2.8"), file.path(getNlDir("dirPolygon"), "SHP_KEN_GADM-2.8"))
+  expect_equal(Rnightlights:::getPolyFnamePath(ctryCode = "KEN", gadmVersion = "2.8"), file.path(getNlDir("dirPolygon"), "POLY_KEN_GADM-2.8-SHPZIP"))
   
-  expect_equal(Rnightlights:::getPolyFnameZip("KEN", "2.8"), file.path(getNlDir("dirPolygon"), "SHP_KEN_GADM-2.8.zip"))
+  expect_equal(Rnightlights:::getPolyFnameZip(ctryCode = "KEN", gadmVersion = "2.8"), file.path(getNlDir("dirPolygon"), "POLY_KEN_GADM-2.8-SHPZIP.zip"))
   
-  expect_equal(Rnightlights:::existsPolyFnamePath("KEN"), file.exists(file.path(file.path(getNlDir("dirPolygon"), "SHP_KEN_GADM-2.8"))))
+  expect_equal(Rnightlights:::existsPolyFnameZip(ctryCode = "KEN"), file.exists(file.path(file.path(getNlDir("dirPolygon"), "POLY_KEN_GADM-2.8-SHPZIP.zip"))))
   
-  expect_equal(Rnightlights:::existsPolyFnameZip("KEN"), file.exists(file.path(getNlDir("dirPolygon"), "SHP_KEN_GADM-2.8.zip")))
+  expect_equal(Rnightlights:::existsPolyFnameRDS(ctryCode = "KEN"), file.exists(file.path(file.path(getNlDir("dirPolygon"), "POLY_KEN_GADM-2.8-SHPZIP.rds"))))
   
-  expect_equal(Rnightlights:::getCtryNlDataFnamePath("KEN", "KEN_adm0", gadmVersion = "2.8"), file.path(file.path(getNlDir("dirNlData"), "NL_DATA_KEN_ADM0_GADM-2.8.csv")))
+  expect_equal(Rnightlights:::existsPolyFnameZip(ctryCode = "KEN"), file.exists(file.path(getNlDir("dirPolygon"), "SHP_KEN_GADM-2.8.zip")))
   
-  expect_equal(Rnightlights:::getCtryRasterOutputFname("KEN","VIIRS.M","201204",gadmVersion = "2.8"), "NL_KEN_VIIRS.M_201204_GADM-2.8.tif")
+  expect_equal(Rnightlights:::getCtryNlDataFnamePath(ctryCode = "KEN", admLevel = "KEN_adm0", gadmVersion = "2.8"), file.path(file.path(getNlDir("dirNlData"), "NL_DATA_KEN_ADM0_GADM-2.8-SHPZIP.csv")))
   
-  expect_equal(Rnightlights:::getCtryRasterOutputFnamePath("KEN","VIIRS.M","201204", "3.6"), file.path(getNlDir("dirRasterOutput"), "NL_KEN_VIIRS.M_201204_GADM-3.6.tif"))
+  expect_equal(Rnightlights:::getCtryRasterOutputFname(ctryCode = "KEN", nlType = "VIIRS.M", nlPeriod = "201204", gadmVersion = "2.8"), "NL_KEN_VIIRS.M_201204_VCMCFG_GADM-2.8-SHPZIP.tif")
+  
+  expect_equal(Rnightlights:::getCtryRasterOutputFnamePath(ctryCode = "KEN", nlType = "VIIRS.M", nlPeriod = "201204", "3.6"), file.path(getNlDir("dirRasterOutput"), "NL_KEN_VIIRS.M_201204_3.6_GADM-3.6-SHPZIP.tif"))
   
 })
--- tests/testthat/test-nlurl.R
+++ tests/testthat/test-nlurl.R
@@ -14,7 +14,7 @@ test_that("nlUrls work", {
   nlUrlVIIRS.Y_2016<- Rnightlights:::getNlUrlVIIRS(nlPeriod = "2016", tileNum = 1, nlType = "VIIRS.Y")
   
   expect_equal(nlUrlOLS.Y_2012, "https://www.ngdc.noaa.gov/eog/data/web_data/v4composites/F182012.v4.tar", fixed=TRUE)
-  expect_equal(nlUrlVIIRS.D_CurrMonth1st, paste0("https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/mosaics//20190701/SVDNB_npp_d", currMonth1st, ".d.75N180W.rade9.tif"), fixed=TRUE)
-  expect_equal(nlUrlVIIRS.M_201204, "https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/dnb_composites/v10//201204/vcmcfg/SVDNB_npp_20120401-20120430_75N180W_vcmcfg_v10_c201605121456.tgz", fixed=TRUE)
-  expect_equal(nlUrlVIIRS.Y_2016, "https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/dnb_composites/v10//2016/SVDNB_npp_20160101-20161231_75N180W_v10_c201807311200.tgz", fixed=TRUE)
+  expect_equal(nlUrlVIIRS.D_CurrMonth1st, paste0("https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/mosaics//", currMonth1st, "/SVDNB_npp_d", currMonth1st, ".d.75N180W.rade9.tif"), fixed=TRUE)
+  expect_equal(nlUrlVIIRS.M_201204, "https://eogdata.mines.edu/wwwdata/viirs_products/dnb_composites/v10//201204/vcmcfg/SVDNB_npp_20120401-20120430_75N180W_vcmcfg_v10_c201605121456.tgz", fixed=TRUE)
+  expect_equal(nlUrlVIIRS.Y_2016, "https://eogdata.mines.edu/wwwdata/viirs_products/dnb_composites/v10//2016/SVDNB_npp_20160101-20161231_75N180W_v10_c201807311200.tgz", fixed=TRUE)
 })
